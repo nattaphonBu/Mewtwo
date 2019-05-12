d411ef38578e4540d5a11b9aad5e0332528bd09e
godzilowen <owen1491@gmail.com>
2019-05-12 11:31:34 +0700

    model controller

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -32,17 +32,71 @@ class Welcome extends CI_Controller {
 	}
 
 	public function insert(){
-		// $studentid = isset($_GET['studentid'])?$_GET['studentid']:"";
-		$studentid  = isset($_POST['studentid'])?$_POST['studentid']:"";
-		$coursename = isset($_POST['coursename'])?$_POST['coursename']:"";
-		$name = isset($_POST['name'])?$_POST['name']:"";
-		$courseid = isset($_POST['courseid'])?$_POST['courseid']:"";
+		// $studentid = isset($_GET['studentid'])?$_GET['studentid']:""
+		$nameActivity = isset($_POST['nameActivity'])?$_POST['activityName']:"";
+		$start_date = isset($_POST['start_date'])?$_POST['startDate']:"";
+		$final_date = isset($_POST['final_date'])?$_POST['finalDate']:"";
+		$start_time = isset($_POST['start_time'])?$_POST['startTime']:"";
+		$final_time = isset($_POST['final_time'])?$_POST['finalTime']:"";
 		$year = isset($_POST['year'])?$_POST['year']:"";
 		$term = isset($_POST['term'])?$_POST['term']:"";
-		$grade = isset($_POST['grade'])?$_POST['grade']:"";
+		$responsible = isset($_POST['responsible'])?$_POST['responsible']:"";
 		// $pass = isset($_GET['password'])?$_GET['password']:"";
-		$this->load->model('UserModel');
-		$result = $this->UserModel->insert($studentid, $coursename,$name,  $courseid, $year, $term, $grade);
+		$this->load->model('activity');
+		$result = $this->activity->create($nameActivity, $start_date, $final_date, $start_time, $final_time, $year, $term, $responsible);
+		if($result){
+			if($result){
+				echo "<script>alert('บันทึกสำเร็จ');
+					window.location.href='getdata';
+					</script>";
+					$this->load->view('showdata');
+			}else{
+				echo "<script>alert('บันทึกไม่สำเร็จ');
+					window.location.href='getdata';
+					</script>";
+					$this->load->view('student');
+			}
+			
+			
+		}
+	}
+
+	public function update(){
+		// $studentid = isset($_GET['studentid'])?$_GET['studentid']:""
+		$nameActivity = isset($_POST['nameActivity'])?$_POST['activityName']:"";
+		$start_date = isset($_POST['start_date'])?$_POST['startDate']:"";
+		$final_date = isset($_POST['final_date'])?$_POST['finalDate']:"";
+		$start_time = isset($_POST['start_time'])?$_POST['startTime']:"";
+		$final_time = isset($_POST['final_time'])?$_POST['finalTime']:"";
+		$year = isset($_POST['year'])?$_POST['year']:"";
+		$term = isset($_POST['term'])?$_POST['term']:"";
+		$responsible = isset($_POST['responsible'])?$_POST['responsible']:"";
+		// $pass = isset($_GET['password'])?$_GET['password']:"";
+		$this->load->model('activity');
+		$result = $this->activity->update($nameActivity, $start_date, $final_date, $start_time, $final_time, $year, $term, $responsible);
+		if($result){
+			if($result){
+				echo "<script>alert('บันทึกสำเร็จ');
+					window.location.href='getdata';
+					</script>";
+					$this->load->view('showdata');
+			}else{
+				echo "<script>alert('บันทึกไม่สำเร็จ');
+					window.location.href='getdata';
+					</script>";
+					$this->load->view('student');
+			}
+			
+			
+		}
+	}
+
+	public function cancel(){
+		// $studentid = isset($_GET['studentid'])?$_GET['studentid']:""
+		$status = isset($_POST['status'])?$_POST['status']:"";
+		// $pass = isset($_GET['password'])?$_GET['password']:"";
+		$this->load->model('activity');
+		$result = $this->activity->cancel($status);
 		if($result){
 			if($result){
 				echo "<script>alert('บันทึกสำเร็จ');
--- application/models/activity.php
+++ application/models/activity.php
@@ -1,14 +1,15 @@
 <?php
 class Activity extends CI_Model
 {
-    public function create($id_activity, $nameActivity, $day, $time, $year, $term, $responsible)
+    public function create($nameActivity, $start_date, $final_date, $start_time, $final_time, $year, $term, $responsible)
     {
         $data = array(
-            'activityID' => $id_activity,
+            'activityID' => null,
             'activityName' => $nameActivity,
             'startDate' => $start_date,
             'finalDate' => $final_date,
-            'time' => $time,
+            'startTime' => $start_time,
+            'finalTime' => $final_time,
             'year' => $year,
             'term' => $term,
             'responsible' => $responsible,
@@ -16,6 +17,35 @@ class Activity extends CI_Model
         
         );
         $this->db->insert('activity', $data);
+        return ($this->db->affected_rows() != 1) ? false : true;
+    }
+
+    public function update($nameActivity, $start_date, $final_date, $start_time, $final_time, $year, $term, $responsible)
+    {
+        $data = array(
+            'activityID' => null,
+            'activityName' => $nameActivity,
+            'startDate' => $start_date,
+            'finalDate' => $final_date,
+            'startTime' => $start_time,
+            'finalTime' => $final_time,
+            'year' => $year,
+            'term' => $term,
+            'responsible' => $responsible,
+            'status' => 1,
+        
+        );
+        $this->db->update('activity', $data);
+        return ($this->db->affected_rows() != 1) ? false : true;
+    }
+
+    public function cancel($status)
+    {
+        $data = array(
+            'status' => 0,
+        
+        );
+        $this->db->cancel('activity', $data);
         return ($this->db->affected_rows() != 1) ? false : true;
     }
     
